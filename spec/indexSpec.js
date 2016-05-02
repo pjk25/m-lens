2c4f8dd9a3ca243e8c02f956be3ebaf22916f508
Phil <pjk25@cornell.edu>
2016-05-02 22:59:10 +0000

    Refactor lens

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,30 +1,57 @@
 import m from "mori";
-import {view, update, at} from "../src/index.js"
+import { view, update, at } from "../src/index.js"
 
 describe('m-lens', function() {
-    describe('at', function() {
-        it('returns a lens that focuses on a property of a structure', function() {
-            let data = m.toClj({
-                vector: [1, 2, 3],
-                map: {
+    let data;
+
+    beforeEach(function() {
+        data = m.toClj({
+            map: {
+                vector: [4, 5, 6],
+                nested: {
                     one: 1,
                     two: 2
                 }
-            });
+            },
+            vector: [1, 2, 3],
+        });
+    });
 
-            let lens = at('vector');
+    describe('at', function() {
+        it('returns a lens that focuses on a property of a structure', function() {
+            let lens = at(['vector']);
 
             expect(m.toJs(view(data, lens)))
                 .toEqual([1, 2, 3]);
 
             expect(m.toJs(update(data, lens, m.last)))
                 .toEqual({
-                    vector: 3,
                     map: {
-                        one: 1,
-                        two: 2
-                    }
-                })
+                        vector: [4, 5, 6],
+                        nested: {
+                            one: 1,
+                            two: 2
+                        }
+                    },
+                    vector: 3
+                });
+
+            lens = at(['map', 'nested']);
+
+            expect(m.toJs(view(data, lens)))
+                .toEqual({
+                    one: 1,
+                    two: 2
+                });
+
+            expect(m.toJs(update(data, lens, m.constantly(3))))
+                .toEqual({
+                    map: {
+                        vector: [4, 5, 6],
+                        nested: 3
+                    },
+                    vector: [1, 2, 3]
+                });
         });
     });
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import m from "mori";
 
-export function at(key) {
-    return function() {
-        return key;
-    }
+export function at(path) {
+    return [[path, []]];
 }
 
 export function view(data, lens) {
-    return m.get(data, lens());
+    return m.reduce(function(acc, [from, to]) {
+        const value = m.getIn(data, from);
+        if (to.length) {
+            return m.assocIn(acc, to, value);
+        } else if (m.isVector(value)) {
+            return value;
+        } else {
+            return m.merge(acc, value);
+        }
+    }, m.hashMap(), lens);
 }
 
 export function update(data, lens, f) {
-    return m.assoc(data, lens(), f(view(data, lens)));
+    const value = f(view(data, lens));
+    return m.reduce(function(acc, [from, to]) {
+        if (to.length) {
+            return m.assocIn(data, from, m.getIn(value, to));
+        } else {
+            return m.assocIn(data, from, value);
+        }
+    }, data, lens)
 }
